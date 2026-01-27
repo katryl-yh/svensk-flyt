ca5c1441040d2330ae7e29f281644374a013d0be
Katrin katryl-yh <katrin@rylander.eu>
2026-01-27 22:11:19 +0100

    feat(marts): create dim_airline dimension table, extract unique airlines from int_flights

--- dbt/models/marts/airline/dim_airline.sql
+++ dbt/models/marts/airline/dim_airline.sql
@@ -1,1 +1,19 @@
--- TODO: Create airline dimension
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with airlines as (
+    select distinct
+        airline_iata,
+        airline_name
+    from {{ ref('int_flights') }}
+    where airline_iata is not null
+)
+
+select
+    {{ dbt_utils.generate_surrogate_key(['airline_iata']) }} as airline_key,
+    airline_iata,
+    airline_name
+from airlines
